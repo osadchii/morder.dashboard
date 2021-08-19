fbd9682b3d7b8cf339dc58cbabacb4fee0ab8b2b
a.osadchii <roflandcom@gmail.com>
2021-08-19 08:22:17 +0300

    product table to component

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -1,24 +1,10 @@
 import {DashboardProps} from "../Dashboard/Dashboard.props";
-import {
-    Container,
-    Paper,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    Typography,
-    withStyles
-} from "@material-ui/core";
+import {Container, Typography} from "@material-ui/core";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Avatar from "@material-ui/core/Avatar";
-import {ChangeEvent, useCallback, useEffect, useState} from "react";
-import {AllInbox} from "@material-ui/icons";
+import {ProductTable} from "./ProductTable";
 import {makeStyles} from "@material-ui/core/styles";
-import Pagination from '@material-ui/lab/Pagination';
-import {ProductModel, ProductType} from "../../services/product/product.model";
-import {ProductService} from "../../services/product/product.service";
+import Avatar from "@material-ui/core/Avatar";
+import {AllInbox} from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -42,85 +28,14 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-const StyledTableCell = withStyles((theme) => ({
-    head: {
-        backgroundColor: theme.palette.primary.main,
-        color: theme.palette.common.white,
-    },
-    body: {
-        fontSize: 15,
-    },
-}))(TableCell);
-
-const StyledTableRow = withStyles((theme) => ({
-    root: {
-        '&:hover': {
-            backgroundColor: theme.palette.action.hover
-        }
-    },
-}))(TableRow);
-
-function readableProductType(productType: ProductType): string{
-    switch (productType){
-        case "Piece":
-            return "Штучный товар";
-        case "Tobacco":
-            return "Табачная продукция";
-        case "Alcohol":
-            return "Алкогольная продукция";
-        case "Weight":
-            return "Весовой товар";
-    }
-}
-
 export const Catalog = ({token, logout}: DashboardProps): JSX.Element => {
 
     const classes = useStyles();
 
-    const pageSize = 48;
-
-    const [errorMessage, setErrorMessage] = useState('');
-    const [page, setPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(15);
-    const [rows, setRows] = useState([] as ProductModel[]);
-    const [loading, setLoading] = useState(true);
-
-    const getProducts = async () => {
-        setLoading(true);
-        try {
-            const products = await ProductService.getProductPage(token, page, pageSize);
-            setRows(products.items);
-            setTotalPages(products.count / pageSize)
-        } catch (error) {
-            const {statusCode} = error.response.data;
-            let message = '';
-            if (statusCode === 401
-                || statusCode === 403) {
-                logout();
-            } else {
-                message = error.toString();
-            }
-
-            setErrorMessage(message);
-        }
-        setLoading(false);
-    }
-
-    const getProductsCallback = useCallback(async () => {
-        await getProducts();
-    }, [token, page]);
-
-    useEffect(() => {
-        getProductsCallback();
-    }, [getProductsCallback]);
-
-    const handleChange = async (event: ChangeEvent<unknown>, value: number) => {
-        setPage(value);
-    };
-
     return (
         <Container component="div" maxWidth={"xl"}>
             <CssBaseline/>
+
             <div className={classes.paper}>
                 <Avatar className={classes.avatar}>
                     <AllInbox/>
@@ -128,39 +43,9 @@ export const Catalog = ({token, logout}: DashboardProps): JSX.Element => {
                 <Typography component="h1" variant="h5">
                     Каталог
                 </Typography>
-                <Typography color={"error"}>
-                    {errorMessage}
-                </Typography>
-                <TableContainer component={Paper}>
-                    <Table className={classes.table} aria-label="customized table">
-                        <TableHead>
-                            <TableRow>
-                                <StyledTableCell>Наименование</StyledTableCell>
-                                <StyledTableCell align="right">Артикул</StyledTableCell>
-                                <StyledTableCell align="right">Штрихкод</StyledTableCell>
-                                <StyledTableCell align="right">Бренд</StyledTableCell>
-                                <StyledTableCell align="right">Тип товара</StyledTableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {rows.map((row) => (
-                                <StyledTableRow key={row.name}>
-                                    <StyledTableCell component="th" scope="row">
-                                        {row.name}
-                                    </StyledTableCell>
-                                    <StyledTableCell align="right">{row.articul}</StyledTableCell>
-                                    <StyledTableCell align="right">{row.barcode}</StyledTableCell>
-                                    <StyledTableCell align="right">{row.brand}</StyledTableCell>
-                                    <StyledTableCell align="right">{readableProductType(row.productType)}</StyledTableCell>
-                                </StyledTableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-
-                <div className={classes.root}>
-                    <Pagination count={totalPages} color="primary" page={page} onChange={handleChange} disabled={loading} />
-                </div>
+                <ProductTable
+                    token={token}
+                    logout={logout}/>
             </div>
         </Container>
     );
---
+++ src/components/Catalog/ProductTable.tsx
@@ -0,0 +1,160 @@
+import {
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography,
+    withStyles,
+} from "@material-ui/core";
+import Pagination from "@material-ui/lab/Pagination";
+import {ChangeEvent, useCallback, useEffect, useState} from "react";
+import {ProductModel, ProductType} from "../../services/product/product.model";
+import {ProductService} from "../../services/product/product.service";
+import {makeStyles} from "@material-ui/core/styles";
+import {DashboardProps} from "../Dashboard/Dashboard.props";
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(2),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.primary.main,
+    },
+    table: {
+        minWidth: 700,
+    },
+    root: {
+        '& > *': {
+            marginTop: theme.spacing(4),
+            marginBottom: theme.spacing(2),
+        },
+    },
+}));
+
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.common.white,
+    },
+    body: {
+        fontSize: 13,
+    },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+    root: {
+        '&:hover': {
+            backgroundColor: theme.palette.action.hover
+        }
+    },
+}))(TableRow);
+
+function readableProductType(productType: ProductType): string {
+    switch (productType) {
+        case "Piece":
+            return "Штучный товар";
+        case "Tobacco":
+            return "Табачная продукция";
+        case "Alcohol":
+            return "Алкогольная продукция";
+        case "Weight":
+            return "Весовой товар";
+    }
+}
+
+export const ProductTable = ({token, logout}: DashboardProps): JSX.Element => {
+
+    const classes = useStyles();
+
+    const pageSize = 48;
+
+    const [errorMessage, setErrorMessage] = useState('');
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(15);
+    const [rows, setRows] = useState([] as ProductModel[]);
+    const [loading, setLoading] = useState(true);
+
+    const getProducts = async () => {
+        setLoading(true);
+        try {
+            const products = await ProductService.getProductPage(token, page, pageSize);
+            setRows(products.items);
+            setTotalPages(Math.ceil(products.count / pageSize));
+        } catch (error) {
+            const {statusCode} = error.response.data;
+            let message = '';
+            if (statusCode === 401
+                || statusCode === 403) {
+                logout();
+            } else {
+                message = error.toString();
+            }
+
+            setErrorMessage(message);
+        }
+        setLoading(false);
+    }
+
+    const getProductsCallback = useCallback(async () => {
+        await getProducts();
+    }, [token, page]);
+
+    useEffect(() => {
+        getProductsCallback();
+    }, [getProductsCallback]);
+
+    const handleChange = async (event: ChangeEvent<unknown>, value: number) => {
+        setPage(value);
+    };
+
+    return (
+        <>
+            <Typography color={"error"}>
+                {errorMessage}
+            </Typography>
+            <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="customized table">
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell>Наименование</StyledTableCell>
+                            <StyledTableCell align="right">Артикул</StyledTableCell>
+                            <StyledTableCell align="right">Штрихкод</StyledTableCell>
+                            <StyledTableCell align="right">Бренд</StyledTableCell>
+                            <StyledTableCell align="right">Тип</StyledTableCell>
+                            <StyledTableCell align="right">Цена</StyledTableCell>
+                            <StyledTableCell align="right">Остаток</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <StyledTableRow key={row.name}>
+                                <StyledTableCell component="th" scope="row">
+                                    {row.name}
+                                </StyledTableCell>
+                                <StyledTableCell align="right">{row.articul}</StyledTableCell>
+                                <StyledTableCell align="right">{row.barcode}</StyledTableCell>
+                                <StyledTableCell align="right">{row.brand}</StyledTableCell>
+                                <StyledTableCell
+                                    align="right">{readableProductType(row.productType)}</StyledTableCell>
+                                <StyledTableCell align="right">{row.price}</StyledTableCell>
+                                <StyledTableCell align="right">{row.stock}</StyledTableCell>
+                            </StyledTableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+            <div className={classes.root}>
+                <Pagination count={totalPages} color="primary" page={page} onChange={handleChange}
+                            disabled={loading}/>
+            </div>
+        </>
+    );
+}
