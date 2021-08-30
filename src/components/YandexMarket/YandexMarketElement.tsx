6480d9f55d4f0e754f6241436bfdbfb17f2371a5
a.osadchii <roflandcom@gmail.com>
2021-08-30 21:50:45 +0300

    start of yandex market element form

--- src/components/YandexMarket/YandexMarketElement.tsx
+++ src/components/YandexMarket/YandexMarketElement.tsx
@@ -1,12 +1,186 @@
-import { useParams } from 'react-router-dom';
+import { Controller, useForm } from 'react-hook-form';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import { Brightness1 } from '@material-ui/icons';
+import { YandexMarketModel } from '../../services/yandexmarket/yandexmarket.model';
+import { useHistory, useParams } from 'react-router-dom';
+import { useCallback, useEffect } from 'react';
+import { YandexMarketService } from '../../services/yandexmarket/yandexmarket.service';
+import { AxiosError } from 'axios';
+import { ApiService } from '../../services/api.service';
+import { Checkbox, FormControlLabel } from '@material-ui/core';
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(2),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
+
+const defaultValues: YandexMarketModel = {
+  _id: '',
+  active: false,
+  feedGenerationInterval: 0,
+  lastFeedGeneration: new Date(),
+  minimalPrice: 0,
+  name: '',
+  nullifyStocks: false,
+  productTypes: [],
+};
 
 export const YandexMarketElement = (): JSX.Element => {
 
+  const history = useHistory();
+
+  const classes = useStyles();
   const params = useParams();
+
   const { id } = params as typeof params & { id: string };
+
+  const { control, handleSubmit, reset } = useForm<YandexMarketModel>({ defaultValues });
+  const onSubmit = handleSubmit(async (data: YandexMarketModel) => {
+    await YandexMarketService.update(data)
+      .catch((error: AxiosError) => {
+        ApiService.catchFetchError(error, history.push);
+      });
+  });
+
+  const getItemCallBack = useCallback(async () => {
+    const item = await YandexMarketService.getById(id)
+      .catch((error: AxiosError) => {
+        ApiService.catchFetchError(error, history.push);
+      });
+
+    if (item) {
+      reset(item);
+    }
+  }, [reset, id]);
+
+  useEffect(() => {
+    getItemCallBack();
+  }, [id]);
+
   return (
-    <>{id}</>
+    <Container component='div' maxWidth={'md'}>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <Brightness1 />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          Yandex.Market
+        </Typography>
+        <form className={classes.form} noValidate
+              onSubmit={onSubmit}
+              method={'post'}>
+          <FormControlLabel
+            label='Активно'
+            control={
+              <Controller
+                control={control}
+                name={'active'}
+                render={
+                  ({ field }) =>
+                    <Checkbox
+                      color='primary'
+                      checked={field.value}
+                      {...field}
+                    />
+                }
+              />
+            }
+          />
+          <FormControlLabel
+            label='Обнулять остатки'
+            control={
+              <Controller
+                control={control}
+                name={'nullifyStocks'}
+                render={
+                  ({ field }) =>
+                    <Checkbox
+                      color='primary'
+                      checked={field.value}
+                      {...field}
+                    />
+                }
+              />
+            }
+          />
+          <Controller
+            control={control}
+            name={'name'}
+            render={
+              ({ field }) =>
+                <TextField
+                  variant={'outlined'}
+                  margin='normal'
+                  required
+                  fullWidth
+                  id='name'
+                  label='Название'
+                  {...field}
+                />
+            }
+            rules={{ required: true }}
+          />
+          <Controller
+            control={control}
+            name={'minimalPrice'}
+            render={
+              ({ field }) =>
+                <TextField
+                  variant={'outlined'}
+                  margin='normal'
+                  required
+                  type={'number'}
+                  fullWidth
+                  label='Минимальная цена'
+                  {...field}
+                  onChange={(e) =>
+                    field.onChange(
+                      Number.isNaN(parseInt(e.target.value))
+                        ? 0
+                        : parseInt(e.target.value),
+                    )
+                  }
+                />
+            }
+            rules={{ min: 0, max: 999999 }}
+          />
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            color='primary'
+            className={classes.submit}
+          >
+            Сохранить
+          </Button>
+        </form>
+      </div>
+    </Container>
   );
 
 };
--- src/services/yandexmarket/yandexmarket.service.ts
+++ src/services/yandexmarket/yandexmarket.service.ts
@@ -4,6 +4,7 @@ import { ApiService } from '../api.service';
 
 export class YandexMarketService {
   private static readonly getListUrl = `${process.env.REACT_APP_API_BASE_URL}/yandexmarket/get`;
+  private static readonly updateUrl = `${process.env.REACT_APP_API_BASE_URL}/yandexmarket/update`;
 
   static async getList(): Promise<YandexMarketModel[]> {
 
@@ -13,6 +14,25 @@ export class YandexMarketService {
       },
     });
     return response.data as typeof response.data & YandexMarketModel[];
+  }
+
+  static async getById(id: string): Promise<YandexMarketModel> {
+    const response = await axios.get(this.getListUrl + `/${id}`, {
+      headers: {
+        ...ApiService.AuthorizationHeaders(),
+      },
+    });
+    return response.data as typeof response.data & YandexMarketModel;
+  }
+
+  static async update(item: YandexMarketModel): Promise<void> {
+    const url = `${this.updateUrl}/${item._id}`;
+    const response = await axios.post(url, item, {
+      headers: {
+        ...ApiService.AuthorizationHeaders(),
+      },
+    });
+    console.log(response);
   }
 
 }
